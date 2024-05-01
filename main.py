633a67e5dc679a4832c5bfccb355e51b34e1f67a
Vasileia Kampouraki <vasokabouraki@gmail.com>
2024-05-01 14:19:15 +0300

    Initial commit of RAG chain

---
+++ main.py
@@ -0,0 +1,55 @@
+import torch
+import streamlit as st
+import time
+
+from utils import load_document, split_doc, create_vectorDB, load_llm, prompt_formatter
+
+torch.cuda.is_available()
+device = "cuda" if torch.cuda.is_available() else "cpu"
+print(f"Available device: {device}")
+
+
+def rag_chain(doc_path, query, reader_model, chunk_size=512, chunk_overlap=50, top_k=5, dtype=torch.bfloat16, device=device):
+    # Load document
+    doc = load_document(doc_path)
+    print(f"The document consists of {len(doc)} pages")
+    
+    # Split document into chunks
+    chunks = split_doc(doc, chunk_size, chunk_overlap)
+    print(f"The document is split into {len(chunks)} chunks.")
+
+    # Create embeddings
+    print("Creation of embeddings and vector Database starts...")
+    vectorDB = create_vectorDB(chunks, chunk_size, device)
+    print("Vector DB is now created!")
+
+    # Load the LLM wrapped in a pipeline
+    READER_LLM, tokenizer = load_llm(reader_model, device, dtype)
+    
+    # Retrieve relevant docs and add them to the prompt template
+    retrieved_docs = vectorDB.similarity_search(query=query, k=top_k)
+    retrieved_docs_text = [doc.page_content for doc in retrieved_docs]  
+    context = f"\n The {top_k} most relevant documents:\n"
+    context += "\n\n".join([f"Chunk {str(i)}:\n" + doc for i, doc in enumerate(retrieved_docs_text)])
+
+    augmented_prompt = prompt_formatter(query=query, tokenizer=tokenizer,
+                            context_items=context)
+
+    answer = READER_LLM(augmented_prompt)[0]["generated_text"]
+
+    return answer, retrieved_docs_text
+
+
+st.title("Hi! This is your digital histopathology ChatBot!")
+
+user_query = st.text_input("Enter your question:", "")
+if st.button("Submit"):
+    try:
+        start_time = time.time()
+        response, relevant_docs = rag_chain(doc_path="thesis.pdf", query= user_query,
+                                  reader_model = 'mistralai/Mistral-7B-Instruct-v0.1')
+        end_time = time.time()
+        execution_time = end_time - start_time
+        st.write(f"{response} \n\n Execution time: {round(execution_time,2)} seconds")
+    except Exception as e:
+        st.write(f"An error occurred: {e}")
---
+++ utils.py
@@ -0,0 +1,104 @@
+from langchain_community.document_loaders import PyPDFLoader
+from langchain_text_splitters import RecursiveCharacterTextSplitter
+from langchain_community.embeddings import HuggingFaceEmbeddings
+from langchain.vectorstores import FAISS
+from langchain_community.vectorstores.utils import DistanceStrategy
+
+from sentence_transformers import SentenceTransformer
+from transformers import AutoTokenizer, AutoModelForCausalLM, BitsAndBytesConfig
+from transformers import pipeline
+
+import streamlit as st
+
+
+def load_document(doc_path):
+    """Load the PDF document and return the document object."""
+    loader = PyPDFLoader(doc_path, extract_images=False)
+    doc = loader.load()
+    #doc[2].page_content
+    return doc
+
+
+def split_doc(doc, chunk_size=512,chunk_overlap=50):
+   """Split the document into chunks."""
+   text_splitter = RecursiveCharacterTextSplitter(
+    chunk_size=chunk_size,
+    chunk_overlap=chunk_overlap, 
+    length_function=len,
+    separators=["\n\n", "\n", ".", " ", ""],
+    add_start_index = True, # includes chunk's start index in metadata
+    strip_whitespace=True # strips whitespace from the start and end of every document
+    )
+   
+   chunks = text_splitter.split_documents(doc)
+   return chunks
+
+
+def create_vectorDB(chunks, chunk_size, device, embedding_model = 'thenlper/gte-small', dist_strategy=DistanceStrategy.COSINE):
+    """Embed the chunks and store them in a vector database."""
+    embeddings = HuggingFaceEmbeddings(model_name=embedding_model, 
+                                    model_kwargs={'device': device}, 
+                                    encode_kwargs={"normalize_embeddings": True} # need to normalize the embeddings for cosine similarity
+                                    )
+    
+    emb_model_max_seq_len = SentenceTransformer(embedding_model).max_seq_length
+    if emb_model_max_seq_len < chunk_size:
+        raise ValueError("The chunk_size is bigger than the max_seq_length of the embedding model! Choose a different embeddings model or change chunk_size.")  
+    
+    vectorDB = FAISS.from_documents(chunks, embeddings, distance_strategy=dist_strategy)
+    return vectorDB    
+
+
+st.cache_resource(ttl = '1d')
+def load_llm(reader_model, device, dtype):
+    """Load the LLM and tokenizer."""
+    quantization_config = BitsAndBytesConfig(load_in_4bit=True) # load in 4bit, compute in float16
+    tokenizer = AutoTokenizer.from_pretrained(reader_model)
+    model = AutoModelForCausalLM.from_pretrained(reader_model,
+                                                device_map=device,
+                                                torch_dtype=dtype,
+                                                quantization_config=quantization_config,
+                                                low_cpu_mem_usage=True  #if we do not have gpu available this can be False
+                                                )
+    
+    READER_LLM = pipeline(
+        model=model,
+        tokenizer=tokenizer,
+        task="text-generation",
+        do_sample=True, # Samples from the probability distribution of the predictions, instead of choosing token with the highest probability.
+        temperature=0.1, 
+        repetition_penalty=1.1,
+        return_full_text=False,
+        max_new_tokens=512,
+    )
+
+    return READER_LLM, tokenizer
+
+
+def prompt_formatter(query, tokenizer, context_items):
+    """Augment the user query with document-based context."""
+    
+    # TODO: Add examples to the template
+    context_augmented_prompt = f"""Based on the information contained in the context items, please answer the query.
+    Give yourself room to think by extracting relevant passages from the context before answering the query.
+    Don't return the thinking, only return the answer.
+    Make sure your answers are as explanatory as possible.
+    If the answer cannot be deduced from the context, say that you are not completely sure before giving the answer.
+    \nNow use the following context items to answer the user query:
+    {context_items}
+
+    \nThis is the user query you need to answer.
+    User query: {query}
+    Answer:"""
+
+    # Create prompt template for instruction-tuned model
+    dialogue_template = [
+        {"role": "user",
+        "content": context_augmented_prompt}
+    ]
+
+    # Apply the chat template
+    prompt = tokenizer.apply_chat_template(conversation=dialogue_template,
+                                          tokenize=False,
+                                          add_generation_prompt=True)
+    return prompt
